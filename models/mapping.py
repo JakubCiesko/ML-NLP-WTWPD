6ba3a3809e63b42f22d4adfb07bc99472c630e0a
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2024-12-04 19:46:31 +0100

    optimizers, orthogonalization

--- models/mapping.py
+++ models/mapping.py
@@ -4,7 +4,10 @@ class Mapping(torch.nn.Module):
     def __init__(self, source_embedding_size, target_embedding_size):
         super().__init__()
         self.W = torch.nn.Linear(source_embedding_size, target_embedding_size, bias=False)
-       
+        self.init_weights()
+
+    def init_weights(self):
+        torch.nn.init.xavier_uniform_(self.W.weight)
+
     def forward(self, source_embeddings):
         return self.W(source_embeddings)
-    
--- models/trainer.py
+++ models/trainer.py
@@ -1,70 +1,60 @@
 import torch
 import torch.nn as nn 
 import torch.nn.functional as F
+import torch.optim as optim
 
 
 
-class Trainer():
-    
-    def __init__(self, gan, optimizer_mapping, optimizer_discriminator, criterion_mapping, criterion_discriminator, scheduler_mapping=None, scheduler_discriminator=None, device='cpu'):
-        self.device = device
-        self.gan = gan.to(device)
-        self.optimizer_mapping = optimizer_mapping
-        self.optimizer_discriminator = optimizer_discriminator
+class Trainer:
+    def __init__(self, gan, dataloader, criterion_mapping, criterion_discriminator, lr_mapping=0.0001, lr_discriminator=0.0001):
+        self.gan = gan
+        self.dataloader = dataloader
         self.criterion_mapping = criterion_mapping
         self.criterion_discriminator = criterion_discriminator
-        self.scheduler_mapping = scheduler_mapping
-        self.scheduler_discriminator = scheduler_discriminator
+        self.optimizer_mapping = optim.Adam(self.gan.mapping.parameters(), lr=lr_mapping)
+        self.optimizer_discriminator = optim.Adam(self.gan.discriminator.parameters(), lr=lr_discriminator)
 
-    def train(self, dataloader, num_epochs, log_interval=10):
-        self.gan.train()
-        smoothing_coef = self.gan.discriminator.smoothing_coeff if self.gan.discriminator.smoothing_coeff else 0
-        discriminator_losses, mapping_losses = [], []
-        for epoch in range(1, num_epochs + 1):
-            mapping_loss_val, discriminator_loss_val = 0, 0
-            for batch_i, (source_emb, target_emb) in enumerate(dataloader, start=1):
-                source_emb, target_emb = source_emb.to(self.device), target_emb.to(self.device)
-                bs = source_emb.size(0)
-                with torch.no_grad():
-                    source_emb = self.gan.mapping(source_emb)
-                discriminator_input = torch.cat([source_emb, target_emb], 0).to(self.device) # concat source & target emb
-                discriminator_labels = torch.FloatTensor(2 * bs).zero_().to(self.device)
-                
-                # Smoothing
-                
-                discriminator_labels[:bs] = 1 - smoothing_coef
-                discriminator_labels[bs:] = smoothing_coef
-                discriminator_labels = discriminator_labels.unsqueeze(1)
-                
-                # Discriminator Training
-                
-                self.gan.discriminator.train()
+    def train(self, num_epochs, log_interval=10):
+        mapping_losses = []
+        discriminator_losses = []
+
+        for epoch in range(num_epochs):
+            mapping_loss_val = 0
+            discriminator_loss_val = 0
+
+            for batch_i, (source_embeddings, target_embeddings) in enumerate(self.dataloader):
+                # Forward pass for discriminator
+                discriminator_input = torch.cat((source_embeddings, target_embeddings), 0)
+                discriminator_labels = torch.cat((torch.ones(source_embeddings.size(0)), torch.zeros(target_embeddings.size(0))), 0)
                 preds = self.gan.discriminator(discriminator_input)
-                loss = self.criterion_discriminator(preds, discriminator_labels)
+                discriminator_loss = self.criterion_discriminator(preds, discriminator_labels)
+
+                # Backward pass and optimization for discriminator
                 self.optimizer_discriminator.zero_grad()
-                loss.backward()
+                discriminator_loss.backward()
                 self.optimizer_discriminator.step()
-                discriminator_loss_val += loss.data.item()
-                
-                # Mapping Training 
-                
-                self.gan.discriminator.eval()
-                self.gan.mapping.train()
-                preds = self.gan.discriminator(discriminator_input)
-                
-                loss = self.criterion_mapping(preds, 1 - discriminator_labels)
-                
+                discriminator_loss_val += discriminator_loss.data.item()
+
+                # Forward pass for mapping
+                mapped_source = self.gan.mapping(source_embeddings)
+                preds = self.gan.discriminator(mapped_source)
+                mapping_loss = self.criterion_mapping(preds, torch.ones(source_embeddings.size(0)))
+
+                # Backward pass and optimization for mapping
                 self.optimizer_mapping.zero_grad()
-                loss.backward()
+                mapping_loss.backward()
                 self.optimizer_mapping.step()
-                mapping_loss_val += loss.data.item()
+                mapping_loss_val += mapping_loss.data.item()
+
+                # Orthogonalization step
                 mapping_tensor = self.gan.mapping.W.weight.data
                 mapping_tensor.copy_((1.01) * mapping_tensor - 0.01 * mapping_tensor.mm(mapping_tensor.t().mm(mapping_tensor)))
                 self.gan.mapping.eval()
 
-            mapping_losses.append(mapping_loss_val / batch_i)
-            discriminator_losses.append(discriminator_loss_val / batch_i)
+            mapping_losses.append(mapping_loss_val / (batch_i + 1))
+            discriminator_losses.append(discriminator_loss_val / (batch_i + 1))
+
             if epoch % log_interval == 0:
                 print(f"Epoch {epoch}:\nDiscriminator loss: {discriminator_losses[-1]}\tMapping loss: {mapping_losses[-1]}")
+
         return discriminator_losses, mapping_losses
-        
